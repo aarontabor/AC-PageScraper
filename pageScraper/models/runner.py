1ac31a9703eae92b01e710c20df82437660d66c7
Aaron W. Tabor <aarontabor50@gmail.com>
2014-06-18 21:07:38 -0300

    Re-organized runner, moved meta class to end

--- pageScraper/models/runner.py
+++ pageScraper/models/runner.py
@@ -2,10 +2,11 @@ from django.db import models
 
 
 class Runner(models.Model):
-  class Meta:
-    app_label = 'pageScraper'
   firstName = models.CharField(max_length=30)
   lastName = models.CharField(max_length=30)
 
   def __unicode__(self):
     return u'<Runner: "%s, %s">' % (self.lastName, self.firstName)
+
+  class Meta:
+    app_label = 'pageScraper'
