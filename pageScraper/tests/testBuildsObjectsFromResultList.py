fe0e0e0de3877d360939c096288750f9bacf7c4e
Aaron W. Tabor <aarontabor50@gmail.com>
2014-06-18 21:45:42 -0300

    Fixed formatting.

--- pageScraper/tests/testBuildsObjectsFromResultList.py
+++ pageScraper/tests/testBuildsObjectsFromResultList.py
@@ -14,18 +14,15 @@ class TestBuildsObjectsFromResultList(TestCase):
     
   def test_it_builds_an_object_for_each_result(self):
     runners = self.subject.build(self.resultsList, self.attributeMapping) 
-
     self.assertEqual(2, len(runners))
 
   def test_it_maps_result_attributes_onto_objects(self):
     runners = self.subject.build(self.resultsList, self.attributeMapping)
     firstRunner = runners[0]
-
     self.assertEqual(firstRunner.name, 'Tim')
 
   def test_it_doesnt_map_non_attributes(self):
     runners = self.subject.build(self.resultsList, self.attributeMapping)
     firstRunner = runners[0]
-
     self.assertNotEqual(firstRunner.name, 'foo')
 
